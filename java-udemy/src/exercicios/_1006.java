edbde0035e52edb218cf8185c3555b1210fa7928
marta-valle <m.limavalle.ti@gmail.com>
2022-10-23 01:56:27 -0300

    atualizando exercicio

--- java-udemy/src/exercicios/_1006.java
+++ java-udemy/src/exercicios/_1006.java
@@ -1,10 +1,41 @@
 package exercicios;
-
+import java.util.Locale;
+import java.util.Scanner;
 public class _1006 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno.
+		 * A seguir, calcule a média do aluno, sabendo que a nota:
+		 *  A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5.
+		 *  Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+		Entrada
+		O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
 
+		Saída
+		Imprima a mensagem "MEDIA" e a média do aluno
+ 		com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade.
+  		Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+		*/
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double media , soma, a1, b1 , c1;
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		
+		a1 = a * 2;
+		b1 = b * 3;
+		c1 = c * 5;
+				
+		soma = a1 + b1 + c1;
+		media = soma/10.0;
+		
+		System.out.printf("MEDIA = %.1f%n" , media);
+
+		sc.close();
 	}
 
 }
